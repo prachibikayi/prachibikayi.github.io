96fb6b5ec0be22350eabf472d674b1903e95d815
prachibikayi <99473144+prachibikayi@users.noreply.github.com>
2022-08-08 17:41:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,33 +3,33 @@ const urlParams = new URLSearchParams(location.search);
 localStorage.setItem('addId', urlParams.get('addId'));
 localStorage.setItem('bikStoreId', urlParams.get('bikStoreId'));
 
-window.onload = function () {
-      console.log(window.location.href, 'LOCATION');
-};
-// (function() {
-//     var pushState = history.pushState;
-//     var replaceState = history.replaceState;
+window.addEventListener('load', (event) => {
+    console.log('The page has fully loaded');
+});
+(function() {
+    var pushState = history.pushState;
+    var replaceState = history.replaceState;
 
-//     history.pushState = function() {
-//         pushState.apply(history, arguments);
-//         window.dispatchEvent(new Event('pushstate'));
-//         window.dispatchEvent(new Event('locationchange'));
-//     };
+    history.pushState = function() {
+        pushState.apply(history, arguments);
+        window.dispatchEvent(new Event('pushstate'));
+        window.dispatchEvent(new Event('locationchange'));
+    };
 
-//     history.replaceState = function() {
-//         replaceState.apply(history, arguments);
-//         window.dispatchEvent(new Event('replacestate'));
-//         window.dispatchEvent(new Event('locationchange'));
-//     };
+    history.replaceState = function() {
+        replaceState.apply(history, arguments);
+        window.dispatchEvent(new Event('replacestate'));
+        window.dispatchEvent(new Event('locationchange'));
+    };
 
-//     window.addEventListener('popstate', function() {
-//         window.dispatchEvent(new Event('locationchange'))
-//     });
-// })();
+    window.addEventListener('popstate', function() {
+        window.dispatchEvent(new Event('locationchange'))
+    });
+})();
 
 
-// // Usage example:
+// Usage example:
 
-// window.addEventListener('locationchange', function(){
-//     console.log('onlocationchange event occurred!');
-// })
+window.addEventListener('locationchange', function(){
+    console.log('onlocationchange event occurred!');
+})
