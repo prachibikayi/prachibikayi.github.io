0b2edf5a02e2425150f920933b0958522bbad15f
prachibikayi <99473144+prachibikayi@users.noreply.github.com>
2022-08-08 16:58:26 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ console.log('Shopify Script Tag Works !!!!!!!!');
 const urlParams = new URLSearchParams(location.search);
 localStorage.setItem('addId', urlParams.get('addId'));
 localStorage.setItem('bikStoreId', urlParams.get('bikStoreId'));
-
+history.pushState(null, null, window.location.pathname);
 window.addEventListener('popstate', function (event) {
 	// The URL changed...
   console.log('Shopify !!!!!!!!');
