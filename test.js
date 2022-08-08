0024c463929647ef2c2c7575aa5f061c2aade66e
prachibikayi <99473144+prachibikayi@users.noreply.github.com>
2022-08-08 14:50:32 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-console.log('HII')
+const urlParams = new URLSearchParams(location.search);
+localStorage.setItem('addId', urlParams.get('addId'));
+localStorage.setItem('bikStoreId', urlParams.get('bikStoreId'));
+window.addEventListener('popstate', function (event) {
+	// The URL changed...
+  console.log('URL CHANGED')
+});
