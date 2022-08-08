1663f90512bc29aad03c392fcf83eccd4bfd4684
prachibikayi <99473144+prachibikayi@users.noreply.github.com>
2022-08-08 17:12:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,16 +2,30 @@ console.log('Shopify Script Tag Works !!!!!!!!');
 const urlParams = new URLSearchParams(location.search);
 localStorage.setItem('addId', urlParams.get('addId'));
 localStorage.setItem('bikStoreId', urlParams.get('bikStoreId'));
-history.pushState(null, null, window.location.pathname);
-window.addEventListener('popstate', function (event) {
-	// The URL changed...
-  console.log('Shopify !!!!!!!!');
-});
-// (function(history){
-//     var pushState = history.pushState;
-//     history.pushState = function(state) {
-//       // YOUR CUSTOM HOOK / FUNCTION
-//       console.log('I am called from pushStateHook');
-//       return pushState.apply(history, arguments);
-//     };
-// })(window.history);
+(function() {
+    var pushState = history.pushState;
+    var replaceState = history.replaceState;
+
+    history.pushState = function() {
+        pushState.apply(history, arguments);
+        window.dispatchEvent(new Event('pushstate'));
+        window.dispatchEvent(new Event('locationchange'));
+    };
+
+    history.replaceState = function() {
+        replaceState.apply(history, arguments);
+        window.dispatchEvent(new Event('replacestate'));
+        window.dispatchEvent(new Event('locationchange'));
+    };
+
+    window.addEventListener('popstate', function() {
+        window.dispatchEvent(new Event('locationchange'))
+    });
+})();
+
+
+// Usage example:
+
+window.addEventListener('locationchange', function(){
+    console.log('onlocationchange event occurred!');
+})
