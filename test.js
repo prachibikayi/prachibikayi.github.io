36e6c05e76c1035abc3f556f3ddbf52c800d537e
prachibikayi <99473144+prachibikayi@users.noreply.github.com>
2022-08-08 15:50:55 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ console.log('new script called')
 const urlParams = new URLSearchParams(location.search);
 localStorage.setItem('addId', urlParams.get('addId'));
 localStorage.setItem('bikStoreId', urlParams.get('bikStoreId'));
-(function(history){
-    var pushState = history.pushState;
-    history.pushState = function(state) {
-      // YOUR CUSTOM HOOK / FUNCTION
-      console.log('I am called from pushStateHook');
-      return pushState.apply(history, arguments);
-    };
-})(window.history);
+// (function(history){
+//     var pushState = history.pushState;
+//     history.pushState = function(state) {
+//       // YOUR CUSTOM HOOK / FUNCTION
+//       console.log('I am called from pushStateHook');
+//       return pushState.apply(history, arguments);
+//     };
+// })(window.history);
