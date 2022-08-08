1c2e48187f91c589f11df1d82e4399b8f88f0f7a
prachibikayi <99473144+prachibikayi@users.noreply.github.com>
2022-08-08 15:41:09 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 const urlParams = new URLSearchParams(location.search);
 localStorage.setItem('addId', urlParams.get('addId'));
 localStorage.setItem('bikStoreId', urlParams.get('bikStoreId'));
-window.addEventListener('popstate', function (event) {
-	// The URL changed...
-  console.log('URL CHANGED')
-});
+(function(history){
+    var pushState = history.pushState;
+    history.pushState = function(state) {
+      // YOUR CUSTOM HOOK / FUNCTION
+      console.log('I am called from pushStateHook');
+      return pushState.apply(history, arguments);
+    };
+})(window.history);
